a7d0a52d248225e0c2e1e2ca42dc3f4b68d4c34f
Moslem Hadi <m.hady@avasosoft.com>
2023-04-15 21:06:52 +0330

    added infrastructure service registry

---
+++ src/Product/Infrastructure/ConfigureServices.cs
@@ -0,0 +1,46 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Identity;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Interceptors;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class ConfigureServices
+{
+    private const string databaseName = "dbName";
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<AuditableEntitySaveChangesInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString(databaseName),
+            builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+ 
+        services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+        services.AddScoped<ApplicationDbContextInitialiser>();
+
+        services.AddDefaultIdentity<ApplicationUser>()
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        services.AddIdentityServer()
+            .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+        services.AddTransient<IDateTime, DateTimeService>();
+        services.AddTransient<IIdentityService, IdentityService>();
+
+        services.AddAuthentication()
+            .AddIdentityServerJwt();
+
+        services.AddAuthorization(options =>
+            options.AddPolicy(PolicyNames.CanPurge, policy => policy.RequireRole(RoleNames.Administrator)));
+
+        return services;
+    }
+}
