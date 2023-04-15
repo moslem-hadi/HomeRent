7f72031889ac44b67346fb6bf28c2a5d7fd8a3f8
Moslem Hadi <m.hady@avasosoft.com>
2023-04-15 19:05:26 +0330

    added identity service and mediator dispatch

---
+++ src/Product/Infrastructure/Common/MediatorExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Common;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Common;
+
+public static class MediatorExtensions
+{
+    public static async Task DispatchDomainEvents(this IMediator mediator, DbContext dbContext, CancellationToken cancellationToken)
+    {
+        var entities = dbContext.ChangeTracker
+            .Entries<BaseEntity>()
+            .Where(a => a.Entity.DomainEvents.Any())
+            .Select(a => a.Entity);
+
+        var domainEvents = entities
+            .SelectMany(a => a.DomainEvents)
+            .ToList();
+
+        entities.ToList().ForEach(a => a.ClearDomainEvents());
+
+        //domainEvents.ForEach(async a => await mediator.Publish(a));
+        foreach (var domainEvent in domainEvents)
+            await mediator.Publish(domainEvent);
+    }
+}
