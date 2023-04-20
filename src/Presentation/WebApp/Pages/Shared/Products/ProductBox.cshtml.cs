92241f6c47d608533a376a55aa8892af5521c279
Moslem Hadi <moslem.hady@gmail.com>
2023-04-20 23:02:27 +0330

    added partial.

---
+++ src/Presentation/WebApp/Pages/Shared/Products/ProductBox.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.ViewModels;
+
+namespace WebApp.Pages.Shared.Products
+{
+    public class ProductBoxModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
